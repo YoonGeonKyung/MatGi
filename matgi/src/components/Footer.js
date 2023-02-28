8252a9a2757a85d096b4492185aba20efc13c050
yoonsaeng <truestar1997@gmail.com>
2023-03-01 07:28:45 +0900

    [feat]: Footer 추가

---
+++ matgi/src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+	height: 65px;
+	position: absolute;
+	bottom: 0;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: #ffffff;
+`;
+
+const FooterText = styled.a`
+	color: gray;
+	&:hover {
+		color: black;
+	}
+`;
+
+const Footer = () => {
+	return (
+		<FooterContainer>
+			<FooterText href="/">GitHub : https://github.com/YoonGeonKyung/MatGi.git</FooterText>
+		</FooterContainer>
+	);
+};
+
+export default Footer;
