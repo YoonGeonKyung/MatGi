72710e3d7ef05080e5b4943303b38d823699312e
yoonsaeng <truestar1997@gmail.com>
2023-03-19 20:33:51 +0900

    [feat]: DummyData 생성

--- matgi/src/components/MapContainer.js
+++ matgi/src/components/MapContainer.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { inputState } from '../store/atom';
+import InputForm from './InputForm';
 
 const { kakao } = window;
+const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
-const MapContainer = () => {
+const MapContainer = props => {
 	const keyword = useRecoilValue(inputState);
 
 	useEffect(() => {
@@ -14,50 +16,40 @@ const MapContainer = () => {
 			level: 3,
 		};
 		const map = new kakao.maps.Map(mapContainer, mapOptions);
-		const ps = new kakao.maps.services.Places();
-		const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 
-		searchPlaces();
+		const geocoder = new kakao.maps.services.Geocoder();
 
-		function searchPlaces() {
-			if (!keyword.replace(/^\s+|\s+$/g, '')) {
-				alert('키워드를 입력해주세요!');
-				return false;
-			}
-
-			ps.keywordSearch(keyword, placesSearchCB);
-		}
-
-		function placesSearchCB(data, status, pagination) {
+		// 주소로 좌표를 검색합니다
+		geocoder.addressSearch(keyword, function (result, status) {
+			// 정상적으로 검색이 완료됐으면
 			if (status === kakao.maps.services.Status.OK) {
-				const bounds = new kakao.maps.LatLngBounds();
+				const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-				for (var i = 0; i < data.length; i++) {
-					displayMarker(data[i]);
-					bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+				for (var i = 0; i < props.dummy.length; i++) {
+					// 마커 이미지의 이미지 크기 입니다
+					const imageSize = new kakao.maps.Size(24, 35);
+
+					// 마커 이미지를 생성합니다
+					const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+					// 결과값으로 받은 위치를 마커로 표시합니다
+					const marker = new kakao.maps.Marker({
+						map: map,
+						position: props.dummy[i].latlng,
+						title: props.dummy[i].title,
+						image: markerImage,
+					});
 				}
 
-				map.setBounds(bounds);
+				// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+				map.setCenter(coords);
 			}
-		}
-
-		function displayMarker(place) {
-			const marker = new kakao.maps.Marker({
-				map: map,
-				position: new kakao.maps.LatLng(place.y, place.x),
-			});
-
-			kakao.maps.event.addListener(marker, 'click', function () {
-				infowindow.setContent(
-					'<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>',
-				);
-				infowindow.open(map, marker);
-			});
-		}
+		});
 	}, [keyword]);
 
 	return (
 		<div>
+			<InputForm />
 			<h2>검색 지도</h2>
 			<div id="myMap" style={{ width: '100%', height: '500px' }}></div>
 		</div>
