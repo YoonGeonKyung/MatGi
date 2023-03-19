407d6429315b3a118390f2057691d4a43b545a2d
unknown <hsy940324@gmail.com>
2023-03-19 21:19:41 +0900

    server init

---
+++ server/matgi/src/main/java/com/example/matgi/MatgiApplication.java
@@ -0,0 +1,13 @@
+package com.example.matgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatgiApplication.class, args);
+	}
+
+}
