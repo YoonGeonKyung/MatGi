e435a8761c8f867fc66556cc7edb4dde0317f3c1
yoonsaeng <truestar1997@gmail.com>
2023-05-21 18:47:49 +0900

    [refactor]: recoil data 추가 및 수정

--- matgi/src/store/atom.js
+++ matgi/src/store/atom.js
@@ -4,3 +4,11 @@ export const inputState = atom({
 	key: 'inputState',
 	default: '',
 });
+
+export const mapLatLng = atom({
+	key: 'mapLatLng',
+	default: {
+		x: 37.2525704606099,
+		y: 127.076905736938,
+	},
+});
